5b573c3475f2bcd61f204f1d42c9892b89dc672a
ebina <ebina.engr@gmail.com>
2023-03-21 21:29:50 +0900

    created API mock

---
+++ pages/api/rooms/[uniqueKey]/detail.ts
@@ -0,0 +1,29 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type ResponseData =
+  | {
+      uniqueKey: string;
+      name: string;
+      status: string;
+      createdAt: string;
+      updatedAt: string;
+    }
+  | { error: string };
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const uniqueKey = req.query.uniqueKey;
+  if (uniqueKey === undefined || uniqueKey instanceof Array) {
+    return res.status(403).json({ error: "invalid_key" });
+  }
+
+  res.status(200).json({
+    uniqueKey: uniqueKey,
+    name: "チャットルーム1",
+    status: "active",
+    createdAt: "2023/03/21-00:00:00",
+    updatedAt: "2023/03/21-00:00:00",
+  });
+}
---
+++ pages/api/rooms/[uniqueKey]/messages.ts
@@ -0,0 +1,49 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export type RequestData = {
+  uniqueKey: string;
+  limit?: number;
+  offset?: number;
+};
+
+type ResponseData =
+  | {
+      uniqueKey: string;
+      totalCount: number;
+      limit: number;
+      offset: number;
+      messages: Array<MesssageData>;
+    }
+  | { error: string };
+
+type MesssageData = {
+  authorName: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+};
+
+const messages: MesssageData[] = [...Array(5)].fill({
+  authorName: "門下生",
+  content: "こんにちは",
+  createdAt: "2023/03/21-00:00:00",
+  updatedAt: "2023/03/21-00:00:00",
+});
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseData>
+) {
+  const uniqueKey = req.query.uniqueKey;
+  if (uniqueKey === undefined || uniqueKey instanceof Array) {
+    return res.status(403).json({ error: "invalid_key" });
+  }
+
+  res.status(200).json({
+    uniqueKey: uniqueKey,
+    totalCount: messages.length,
+    limit: 100,
+    offset: 0,
+    messages: messages,
+  });
+}
