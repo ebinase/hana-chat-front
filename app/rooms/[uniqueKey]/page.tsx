f3cd6c7a9aa50f3c57e582d8ff90770a83d97c9a
ebina <ebina.engr@gmail.com>
2023-03-26 18:30:20 +0900

    [add]チャットルームに適用

--- app/rooms/[uniqueKey]/page.tsx
+++ app/rooms/[uniqueKey]/page.tsx
@@ -1,34 +1,12 @@
-import { Suspense} from 'react'
-import Link from 'next/link';
-import Messages from './messages';
-import Loading from '../../loding';
+'use client';
 
-const Page = async ({ params }: { params: { uniqueKey: string } }) => {
-  return (
-    <div className='min-h-screen flex flex-col'>
-      <header className='bg-fuchsia-900 px-4 py-4 flex justify-between md-flex-col'>
-        <div className='flex gap-3'>
-          <Link href={'/'} className='text-white font-extrabold'>{'<'}</Link>
-          <h1 className='text-xl text-white font-extrabold'>吉田チャット</h1>
-          {params.uniqueKey}
-        </div>
-        <div className='text-white'>
-          メニュー
-        </div>
-      </header>
-      <main className='py-0 px-8 flex flex-col flex-grow '>
-        <div className='flex-grow my-2'>
-          <Suspense fallback={<Loading/>}>
-            <Messages/>
-          </Suspense>
-        </div>
-        <div className='bg-slate-200 py-4 px-6 flex justify-between gap-4'>
-          <input className='grow' type="text" />
-          <button className=''>送信</button>
-        </div>
-      </main>
-    </div>
-  )
-}
+import { NextPage } from 'next';
+import ChatRoom from '../../../src/rooms/components/ChatRoom';
 
-export default Page
+type Pramas = { params: { uniqueKey: string } };
+
+const Page: NextPage<Pramas> = ({ params }) => {
+  return <ChatRoom uniqueKey={params.uniqueKey}></ChatRoom>;
+};
+
+export default Page;
