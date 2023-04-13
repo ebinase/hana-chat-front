dc22a24db54da87f38f866c3b4f2e6185930a97a
ebina <ebina.engr@gmail.com>
2023-04-13 22:19:57 +0900

    [remove]footer削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div>
           {children}
         </div>
-        <Footer></Footer>
       </body>
     </html>
   );
