b309cb2e4036fce2441977391b21fe298dab54c0
ebina <ebina.engr@gmail.com>
2023-04-15 16:56:54 +0900

    [refactor] 不要なstateを削除

--- src/rooms/share/hooks/useUserName.tsx
+++ src/rooms/share/hooks/useUserName.tsx
@@ -1,19 +1,15 @@
 'use client';
 
-import { useState } from 'react';
 import { useCookies } from 'react-cookie';
 
 const useUserName = () => {
   const [cookies, setCookie] = useCookies(['name']);
 
-  const [value, setValue] = useState<string>(cookies.name);
-
   const setName = (name: string) => {
     setCookie('name', name);
-    setValue(name);
   };
 
-  return { name: value, setName };
+  return { name: cookies.name, setName };
 };
 
 export default useUserName;
